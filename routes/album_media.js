783e72372045831a342205e60901c60ac0115aa2
Nancy <nmandujano1@gmail.com>
2017-07-28 13:06:24 -0400

    retrieve_all and retrieve_one controllers up and running seamlessly.
    had to do a mod on the doc provided by sequelize.  they dont support raw queries (in terms of searching for a specific medium in retrieve_one CB) had to send in oobject to search.  Although, I have a feeling that the reason it didnt work correctly was because I was passing in a variable not a hard coded ID to search for.  Ima play with it for a sec.
    
    Anyway, this commit is running a-okay.
    
    UPDATE: nope, raw queries are def deprecated. variable or no variable.  Must do what I did in terms of using an object to search for.

--- routes/album_media.js
+++ routes/album_media.js
@@ -22,23 +22,52 @@ let album_media_routes = function(app) {
 
 // Core callback functions for AlbumMedia 'transfer file'
 
-    function retrieve_all_media(request, reponse){
+    function retrieve_all_media(request, response){
         let album_id = request.params.album_id;
 
-        console.log("getting everything");
+        let find_album = models.Album.findOne({
+            where: {
+                id: album_id
+            }
+        });
+
+         find_album.then(found_album => {
+            found_album.getMedia().then(guts => {
+                response.send(guts);
+                response.statusCode = 200;
+                response.end();
+            });
+        });
     }
 
-    function retrieve_one_medium(request, reponse){
+    function retrieve_one_medium(request, response){
         let album_id = request.params.album_id;
+        let medium_id = request.params.medium_id;
 
-        console.log('getting one');
+        let find_album = models.Album.findOne({
+            where: {
+                id: album_id
+            }
+        });
+
+        find_album.then(found_album => {
+            found_album.getMedia({
+                where: {
+                    id: medium_id
+                }
+            }).then(single_file => {
+                    response.send(single_file);
+                    response.statusCode = 200;
+                    response.end();
+            })
+        });
     }
 
     function create_medium_in_album(request, reponse){
-        let album_id = request.params.album_id;
-        let medium_id = request.params.medium_id;
+        //let album_id = request.params.album_id;
+        //let medium_id = request.params.medium_id;
         console.log(album_id, "album id", medium_id, "medium id");
-        console.log('creating one');
+        //console.log('creating one');
     }
 
     function remove_medium_from_album(request, reponse){
